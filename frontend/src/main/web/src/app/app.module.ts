1e449b44105653a55ae19e3f7f7cfd8e1146b6f6
Lukasz Sakowicz <lukasz.sakowicz217@gmail.com>
2021-03-08 01:10:29 +0100

    Main nav part 2

--- frontend/src/main/web/src/app/app.module.ts
+++ frontend/src/main/web/src/app/app.module.ts
@@ -1,20 +1,11 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-
 import {AppRoutingModule} from '@app/app-routing.module';
 import {AppComponent} from '@app/app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {SharedModule} from '@app/shared/shared.module';
 import {LayoutModule} from '@angular/cdk/layout';
-import { MainNavComponent } from './main-nav/main-nav.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatCardModule } from '@angular/material/card';
-import { MatMenuModule } from '@angular/material/menu';
+import {MainNavComponent} from './main-nav/main-nav.component';
 
 @NgModule({
   declarations: [
@@ -26,15 +17,7 @@ import { MatMenuModule } from '@angular/material/menu';
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MatGridListModule,
-    MatCardModule,
-    MatMenuModule
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/main/web/src/app/main-nav/main-nav.component.ts
+++ frontend/src/main/web/src/app/main-nav/main-nav.component.ts
@@ -1,21 +1,37 @@
-import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import {AfterViewChecked, ChangeDetectorRef, Component, Renderer2} from '@angular/core';
 
 @Component({
   selector: 'app-main-nav',
   templateUrl: './main-nav.component.html',
   styleUrls: ['./main-nav.component.scss']
 })
-export class MainNavComponent {
+export class MainNavComponent implements AfterViewChecked {
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      shareReplay()
-    );
+  // md && gt-md media query
+  mediaQuery = window.matchMedia('(min-width: 960px)');
+  navHidden = true;
+  test = false;
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  toggleNavigation() {
+    this.navHidden = !this.navHidden;
+  }
 
+  constructor(private cdr: ChangeDetectorRef, private renderer: Renderer2) {
+  }
+
+  ngAfterViewChecked() {
+    if (this.mediaQuery.matches) {
+      this.navHidden = true;
+      this.cdr.detectChanges();
+    }
+  }
+
+  toggleDarkMode() {
+    const clazz = 'dark-mode';
+    if (document.body.classList.contains(clazz)) {
+      this.renderer.removeClass(document.body, clazz);
+    } else {
+      this.renderer.addClass(document.body, clazz);
+    }
+  }
 }
